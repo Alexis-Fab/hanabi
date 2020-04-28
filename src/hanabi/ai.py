691ada81b3ee9dfb1952958241c51546bdbfe04f
Alexis-Fab <63000716+Alexis-Fab@users.noreply.github.com>
2020-04-28 15:39:53 +0200

    add first version of Robot_thinking_as_human(AI)
    Cette version fonctionne à 2 joueurs. Elle finit les parties avec un score égal à 5 en faisant presque toujours moins de 3 fautes. Elle pose seulement les 1 et n'utilise pas d'indice de couleur.
    Elle comporte déjà les attributs : have_clue (destiné à beaucoup évoluer), playable, situation_is_risky et last_rep

--- src/hanabi/ai.py
+++ src/hanabi/ai.py
@@ -24,6 +24,8 @@ class AI:
         return list(itertools.chain.from_iterable([hand.cards for hand in self.other_hands]))
 
 
+
+
 class Cheater(AI):
     """
     This player can see his own cards!
@@ -135,3 +137,73 @@ class Cheater(AI):
         print('Cheater is doomed and must discard:', act, myprecious)
         return act
 
+
+class Robot_thinking_as_human(AI):
+    "This robot follows human conventions"
+    "Dans un premier temps, seulement fonctionnel pour des parties à 2 joueurs"
+
+    def play(self):
+        game = self.game
+        print(game.examine_piles())
+        print("Is the situation risky ? ",self.situation_is_risky())
+        (res,clues) = self.have_clue()
+        if res:
+            for ind_card in range(0,len(game.current_hand.cards)):
+                if clues[ind_card][1] == '1':
+                    return("p%d"%(ind_card+1))
+        if game.blue_coins > 0:
+            for ind_hand in range(0,len(self.other_hands)):
+                hand = self.other_hands[ind_hand]
+                for ind_card in range(0,len(hand.cards)):
+                    card = hand.cards[ind_card]
+                    if ((card.number == 1) & (not card.number_clue) & (game.piles[card.color] == 0)):
+                        return ("c1")
+                    if ((card.number == 5) & (not card.number_clue)):
+                       return("c5")                        
+            if game.blue_coins == 8:
+                return("c2")
+        print("Robot should discard")
+        for ind_card in range(0,len(game.current_hand.cards)):
+            if clues[ind_card] == [None,None]:
+              return("d%d"%(ind_card+1))
+        return("d1")
+
+    def have_clue(self): # retourne un booléen et un tableau d'indication
+        game = self.game
+        res = False
+        clues = [[None,None],[None,None],[None,None],[None,None],[None,None]] #tableau de couples (indice couleur,indice nombre), FIX ME : utile ou card.number_clue et card.color_clue suffisent ?
+        for ind_card in range(0,len(game.current_hand.cards)):
+            card = game.current_hand.cards[ind_card]
+            if card.color_clue:
+                res = True
+                clues[ind_card][0] = card.color_clue
+            if card.number_clue:
+                res = True
+                clues[ind_card][1] = card.number_clue
+        return(res,clues)
+
+    def playable(self,card): # retourne si la carte peut être jouée sans risque
+        game = self.game
+        return(card.number == (game.piles[card.color]+1))
+
+    def situation_is_risky(self): # retourne si une carte clef risque d'être défaussée par un partenaire
+        game = self.game
+        for ind_hand in range(0,len(self.other_hands)):
+            hand = self.other_hands[ind_hand]
+            chop_card = hand.cards[0]
+            ind_card = 0
+            while ((chop_card.number_clue != False) & (ind_card<(len(hand.cards)-1))):
+                ind_card += 1
+                chop_card = hand.cards[ind_card]
+            if (ind_card == 4 & chop_card.number_clue):
+                return(self.last_rep(hand.cards[0]))
+            return(self.last_rep(chop_card))
+
+    def last_rep(self,card): # retourne si la carte n'a pas encore été posée et qu'elle est la dernière disponible
+        game = self.game
+        if card.number == 5:
+            return(True)
+        if card.number > 1:
+            return(card in game.discard_pile.cards)
+        if card.number == 1:
+            return(game.discard_pile.cards.count(card) == 2)
