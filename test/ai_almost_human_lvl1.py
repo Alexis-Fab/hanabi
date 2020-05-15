e992d419cb69560408a584bfa80033802b5df7bd
gregoiregllt <62796811+gregoiregllt@users.noreply.github.com>
2020-05-15 14:51:15 +0200

    Update ai_almost_human_lvl1.py

--- test/ai_almost_human_lvl1.py
+++ test/ai_almost_human_lvl1.py
@@ -19,10 +19,10 @@ class Robot_1(AI):
                 hand = self.other_hands[ind_hand]
                 for ind_card in range(0,len(hand.cards)):
                     card = hand.cards[ind_card]
-                    if ((card.number == 1) & (not card.number_clue) & (game.piles[card.color] == 0)):
+                    if ((card.number == 1) & (not card.number_clue[0]) & (game.piles[card.color] == 0)):
                         return ("c1")
-                    if ((card.number == 5) & (not card.number_clue)):
-                       return("c5")                        
+                    if ((card.number == 5) & (not card.number_clue[0])):
+                       return("c5")
             if game.blue_coins == 8:
                 return("c%d"%(randint(2,5)))
         print("Robot should discard")
@@ -37,12 +37,12 @@ class Robot_1(AI):
         clues = [[None,None],[None,None],[None,None],[None,None],[None,None]] #tableau de couples (indice couleur,indice nombre), FIX ME : utile ou card.number_clue et card.color_clue suffisent ?
         for ind_card in range(0,len(game.current_hand.cards)):
             card = game.current_hand.cards[ind_card]
-            if card.color_clue:
+            if card.color_clue[0]:
                 res = True
-                clues[ind_card][0] = card.color_clue
-            if card.number_clue:
+                clues[ind_card][0] = card.color_clue[0]
+            if card.number_clue[0]:
                 res = True
-                clues[ind_card][1] = card.number_clue
+                clues[ind_card][1] = card.number_clue[0]
         return(res,clues)
 
     def playable(self,card): # retourne si la carte peut être jouée sans risque
@@ -55,10 +55,10 @@ class Robot_1(AI):
             hand = self.other_hands[ind_hand]
             chop_card = hand.cards[0]
             ind_card = 0
-            while ((chop_card.number_clue != False) & (ind_card<(len(hand.cards)-1))):
+            while ((chop_card.number_clue[0] != False) & (ind_card<(len(hand.cards)-1))):
                 ind_card += 1
                 chop_card = hand.cards[ind_card]
-                if (ind_card == 4 & chop_card.number_clue):
+                if (ind_card == 4 & int(chop_card.number_clue[0])):
                     return(self.last_rep(hand.cards[0]))
             return(self.last_rep(chop_card))
 
