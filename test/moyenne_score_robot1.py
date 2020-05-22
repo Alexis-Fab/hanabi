916a51af71be8c3fb45117b58b4f063cdfb39b4f
Alexis-Fab <63000716+Alexis-Fab@users.noreply.github.com>
2020-05-22 12:43:26 +0200

    Update moyenne_score_robot1.py

--- test/moyenne_score_robot1.py
+++ test/moyenne_score_robot1.py
@@ -5,21 +5,30 @@ from ai_almost_human_lvl1 import Robot_1
 #cd ..\src & python3 setup.py install --user & cd ..\test	
 
 #from hanabi import my_new_smart_ai
-N=200
+N=500
 scores=[]
+nb_win = 0
 S=0
+failures = 0
 
 for i in range(0,N):
 	game = hanabi.Game(2)  # 2 players
 	game.quiet = True
+	game.only_robots = True
 	ai = Robot_1(game)
 	ai.quiet = True
-	#ai = my_new_smart_ai.RandomAI(game)
+
 
 	# pour jouer toute une partie
 	game.ai = ai
 	game.run()
 
 	scores.append(game.score)
+	if game.score == 0:
+		failures += 1
+	else:
+		nb_win += 1
 	S += game.score
-print(S/N)
+print("average score:",S/N)
+print("failures : ",(failures/N)*100,"%")
+print("average winning score:",S/nb_win)
